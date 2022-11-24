cb8c52bed36d3d28f7329b7ea88f094a47687b76
Paco Benitez <paco.benitez@sfd.gov.sa>
2022-11-24 14:58:53 +0100

    Client Hello World

---
+++ app/src/main/java/eventstore/AccountCreated.java
@@ -0,0 +1,24 @@
+package eventstore;
+
+import java.util.UUID;
+
+public class AccountCreated {
+  private UUID id;
+  private String name;
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
